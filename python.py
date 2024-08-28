f62fe29c44752eafef98c740989b95f57a67311d
isomiki <147840187+isomiki@users.noreply.github.com>
2024-08-28 22:04:08 +0200

    Create python.py

---
+++ python.py
@@ -0,0 +1,18 @@
+# Python Cheat Sheet
+
+# Variables
+variable_name = value
+
+# Functions
+def function_name(parameters):
+    return value
+
+# Conditionals
+if condition:
+    # code
+else:
+    # code
+
+# Loops
+for item in iterable:
+    # code
