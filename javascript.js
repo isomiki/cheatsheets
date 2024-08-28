2e209e70442da6404e672c1ab049a63be8dd988d
isomiki <147840187+isomiki@users.noreply.github.com>
2024-08-28 22:03:38 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// JavaScript Cheat Sheet
+
+// Variables
+let variableName = value; // Declare a variable
+const constantName = value; // Declare a constant
+
+// Functions
+function functionName(parameters) {
+    // Function body
+}
+
+// Conditionals
+if (condition) {
+    // code
+} else {
+    // code
+}
+
+// Loops
+for (let i = 0; i < 10; i++) {
+    // code
+}
