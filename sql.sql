f5096b867441b78f0f69248c45c365289e0e9fd1
isomiki <147840187+isomiki@users.noreply.github.com>
2024-08-28 22:05:02 +0200

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,15 @@
+-- SQL Cheat Sheet
+
+SELECT column1, column2
+FROM table_name
+WHERE condition;
+
+INSERT INTO table_name (column1, column2)
+VALUES (value1, value2);
+
+UPDATE table_name
+SET column1 = value1
+WHERE condition;
+
+DELETE FROM table_name
+WHERE condition;
